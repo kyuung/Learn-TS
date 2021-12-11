a90f5119ae997bfa0a13ef9a3c671486974aaeca
kyuung <mel1521@naver.com>
2021-12-11 13:22:17 +0900

    ✅  learn-generic2

--- class-note/8_generics.ts
+++ class-note/8_generics.ts
@@ -39,3 +39,74 @@ str.split('')
 
 // boolean 이라는 타입을 받아서 쓰겠다고 호출할 때 정의
 const bool = logText<boolean>(true)
+
+
+
+
+// 인터페이스에 제네릭을 선언하는 방법
+interface Dropdown {
+    value: string;
+    selected: boolean;
+}
+
+interface Dropdown2<T> {
+    value: T;
+    selected: boolean;
+}
+
+// const obj: Dropdown = {
+//     value: 10,
+//     selected: false
+// };
+
+const obj2: Dropdown2<number> = {
+    value: 10,
+    selected: true
+}
+
+
+// NOTE : 제네릭에 타입 제한하기 1
+function logTextLength<T>(text: T[]): T[] {
+    console.log(text.length);
+    text.forEach((text)=>{
+        console.log(text)
+    })
+    return text;
+}
+
+logTextLength<string>(['abc', 'hi'])
+
+
+// NOTE : 제네릭에 타입 제한하기 2 - 정의된 타입 이용하기
+interface LengthType {
+    length: number;
+}
+
+// 제네릭으로 받은 타입은 LengthType에서 정의된 하위 타입이다.
+function logTextLength2<T extends LengthType>(text: T):T {
+    text.length;
+    return text;
+}
+
+logTextLength2('abc');
+logTextLength2([1,3,5])
+// logTextLength2(10);
+// logTextLength2({a: 123, b:2456});
+
+
+
+// 제네릭 타입 제한 3 - keyof
+
+// 쇼핑아이템에 있는 키들중에 한가지가 타입이 될 것이당
+interface ShoppingItem {
+    name: string;
+    price: number;
+    stock: number;
+}
+function getShopingItemOption<T extends keyof ShoppingItem>(itemOption: T):T {
+    return itemOption;
+}
+getShopingItemOption(10);
+getShopingItemOption(false);
+
+getShopingItemOption('name');
---
+++ dropdown-generic-example/dropdown-generic.ts
@@ -0,0 +1,35 @@
+interface DropdonwItem<T> {
+  value: T;
+  selected:boolean;
+}
+
+const emails: DropdonwItem<string>[] = [
+  { value: 'naver.com', selected: true },
+  { value: 'gmail.com', selected: false },
+  { value: 'hanmail.net', selected: false },
+];
+
+const numberOfProducts: DropdonwItem<number>[] = [
+  { value: 1, selected: true },
+  { value: 2, selected: false },
+  { value: 3, selected: false },
+];
+
+function createDropdownItem<T>(item: DropdonwItem<T>) {
+  const option = document.createElement('option');
+  option.value = item.value.toString();
+  option.innerText = item.value.toString();
+  option.selected = item.selected;
+  return option;
+}
+
+// NOTE: 드롭 다운 아이템 추가
+emails.forEach(function (email) {
+  const item = createDropdownItem<string>(email);
+  const selectTag = document.querySelector('#email-dropdown');
+  selectTag.appendChild(item);
+});
+
+numberOfProducts.forEach(function (numberOfProduct) {
+  const item = createDropdownItem<number>(numberOfProduct);
+});
